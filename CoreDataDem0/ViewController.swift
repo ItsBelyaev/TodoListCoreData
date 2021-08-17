c4410aa3077836a475d76a02b1a2add2e3eb53e2
down ultra <67225488+ultradown@users.noreply.github.com>
2021-08-17 10:49:34 +0300

    Initial Commit

---
+++ CoreDataDem0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDem0
+//
+//  Created by Daniil Belyaev on 17.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
